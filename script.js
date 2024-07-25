9e8d6b2df5cf85a3e0f28dd66f94fd0720f622a2
Koshak7899 <168594116+Koshak7899@users.noreply.github.com>
2024-07-25 16:11:27 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function generic(){
+	var min = Number(document.getElementById("min_number").value);
+	var max = Number(document.getElementById("max_number").value);
+	var res_input = document.getElementById("result_input");
+	var res;
+	if (min == max) res = min;
+	else if (min > max) res = max;
+	else{
+		res = Math.floor(Math.random() * (max - min));
+		res += min;
+		console.log(res);
+	}
+	res_input.value = res;
+}
+function copy_result(){
+	var res_input = document.getElementById("result_input");
+	res_input.select();
+	document.execCommand("copy");
+}
