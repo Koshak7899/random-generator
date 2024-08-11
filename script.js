4c88e9158e75c3b9a0be000bab3d6032f9556016
Koshak7899 <168594116+Koshak7899@users.noreply.github.com>
2024-08-11 21:55:13 +0400

    Update script.js

--- script.js
+++ script.js
@@ -17,5 +17,5 @@ function copy_result(){
 	res_input.select();
 	document.execCommand("copy");
     let tg = window.Telegram.WebApp;
-    console.log(tg.initDataUnsafe.id);
+    res_input.value = tg.initDataUnsafe.id;
 }
